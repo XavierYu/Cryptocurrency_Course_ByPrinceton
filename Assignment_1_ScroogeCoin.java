813372ff88e90ea07fcfe8613ad319effe86f82d
XavierYu <landover.yu@gmail.com>
2017-10-17 17:26:36 +0800

    Create Assignment_1_ScroogeCoin.java

---
+++ Assignment_1_ScroogeCoin.java
@@ -0,0 +1,17 @@
+// Assignment 1 ScroogeCoin
+
+//Transaction
+//-UniqueID
+//-Input- Hash of transaction contains the previous output
+//     - index of output in the transaction
+//     - and a signature (Isvalid? == verify(pk, msg, signature(sk, msg))
+//-Ouput- public key(addresss being paid)
+//     - value
+
+
+// UTXO.java - unspent transaction output
+//-Hash of transaction that it originates
+//-its Index within that transaction
+
+//UTXOPoll.java
+//UTXO(Current) ---> (Current)Transaction.Output
